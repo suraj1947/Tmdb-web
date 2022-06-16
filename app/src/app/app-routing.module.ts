dff4b3eec0f904403648a0f83b27d2c5fb24abcf
Suraj Pohekar <suraj@incubyte.co>
2022-06-16 16:58:03 +0530

    addding routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TmdbArtistComponent } from './tmdb-artist/tmdb-artist.component';
+import { TmdbComponent } from './tmdb/tmdb.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'tmdb',component:TmdbComponent},
+  {path:'artist',component:TmdbArtistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
